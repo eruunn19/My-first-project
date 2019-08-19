9df41efd228009ffc0bf51ae6fa67ffd7c5a80d1
Robert Murphy <rmurphy@Roberts-MBP.lan1>
2019-08-19 17:18:48 -0400

    Added in location string to location.txt file

--- bash-script.sh
+++ bash-script.sh
@@ -6,3 +6,6 @@ mkdir Games/Diablo-3
 
 touch Games/World-of-Warcraft/location.txt 
 touch Games/Diablo-3/location.txt
+
+echo $(pwd)/Games/World-of-Warcraft/location.txt > Games/World-of-Warcraft/location.txt
+echo $(pwd)/Games/Diablo-3/location.txt > Games/Diablo-3/location.txt
